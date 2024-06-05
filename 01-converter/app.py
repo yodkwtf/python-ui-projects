7cacad15aed4918cc824a4746bd95993a9b1e59c
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-06-05 23:24:39 +0530

    converter app completed

--- 01-converter/app.py
+++ 01-converter/app.py
@@ -1,6 +1,13 @@
 import PySimpleGUI as sg
 
-layout = []
+layout = [
+    [
+        sg.Input(key="-INPUT-"),
+        sg.Spin(["Km to Miles", "Kg to lbs", "Sec to Min"], key="-UNITS-"),
+        sg.Button("Convert", key="-CONVERT-"),
+    ],
+    [sg.Text("Result: ", key="-OUTPUT-")],
+]
 
 window = sg.Window("Converter", layout)
 
@@ -9,5 +16,22 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
+
+    if event == "-CONVERT-":
+        input_value = values["-INPUT-"]
+        unit = values["-UNITS-"]
+
+        if input_value.isnumeric() == False or input_value == "":
+            window["-OUTPUT-"].update("Please enter a valid number")
+            continue
+
+        if unit == "Km to Miles":
+            result = float(input_value) * 0.621371
+        elif unit == "Kg to lbs":
+            result = float(input_value) * 2.20462
+        elif unit == "Sec to Min":
+            result = float(input_value) / 60
+
+        window["-OUTPUT-"].update(f"Result: {round(result, 2)}")
 
 window.close()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Python GUI Projects with PySimpleGUI
+
+A collection of GUI projects built with Python using PySimpleGui module. This repository was created to get hands on practice with some advanced and unfamiliar python code.
+
+## List of Projects
+
+1. [Converter](./01-converter/)
