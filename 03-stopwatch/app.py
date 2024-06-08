3872a9db5555a74d609cbae16f32770bc2ace5f4
yodkwtf <48durgesh.kumar@gmail.com>
2024-06-08 23:56:59 +0530

    basic elements added

--- 03-stopwatch/app.py
+++ 03-stopwatch/app.py
@@ -1,13 +1,13 @@
 import PySimpleGUI as sg
 
-layout = [[]]
+layout = [[sg.Text("time")], [sg.Button("Start"), sg.Button("Stop"), sg.Button("Lap")]]
 
-window = sg.Window("PySimpleGUI Project", layout)
+window = sg.Window("Stopwatch", layout, size=(300, 300), no_titlebar=True)
 
 while True:
     event, values = window.read()
 
-    if event == sg.WIN_CLOSED:
+    if event in (sg.WIN_CLOSED, "Stop"):
         break
 
 window.close()
