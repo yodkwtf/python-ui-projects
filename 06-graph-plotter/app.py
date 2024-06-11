0d0325f5f1727891648cd90d8182d1e08fc77666
yodkwtf <48durgesh.kumar@gmail.com>
2024-06-12 01:00:35 +0530

    graph plotter - completed

--- 06-graph-plotter/app.py
+++ 06-graph-plotter/app.py
@@ -3,24 +3,34 @@ import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.figure
 
-sg.theme("black")
+
+def update_graph(data):
+    axes = fig.axes
+    x = [row[0] for row in data]
+    y = [int(row[1]) for row in data]
+    axes[0].plot(x, y, "r-")
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack()
+
+
 table_data = []
 
 layout = [
     [
         sg.Table(
-            headings=["Observation No.", "Result"],
+            headings=["No.", "Result"],
             values=table_data,
             expand_x=True,
             hide_vertical_scroll=True,
             key="-TABLE-",
+            cols_justification=["center", "center"],
         )
     ],
     [sg.Input(key="-INPUT-", expand_x=True), sg.Button("Add")],
     [sg.Canvas(key="-CANVAS-")],
 ]
 
-window = sg.Window("Graph App", layout)
+window = sg.Window("Graph Plotter", layout, finalize=True)
 
 # Matplotlib
 fig = matplotlib.figure.Figure(figsize=(5, 4))
@@ -41,5 +51,6 @@ while True:
             table_data.append([len(table_data) + 1, float(input_value)])
             window["-TABLE-"].update(values=table_data)
             window["-INPUT-"].update("")
+            update_graph(table_data)
 
 window.close()
