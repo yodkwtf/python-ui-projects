023f207691958eaa06d9082aa1642824b2546388
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-06-14 23:59:14 +0530

    basic layout created

--- 07-image-editor/app.py
+++ 07-image-editor/app.py
@@ -1,6 +1,24 @@
 import PySimpleGUI as sg
 
-layout = [[]]
+control_col = sg.Column(
+    [
+        [
+            sg.Frame(
+                "Contrast",
+                layout=[[sg.Slider((0, 10), orientation="h", key="-BRIGHTNESS-")]],
+            )
+        ],
+        [
+            sg.Checkbox("Emboss", key="-EMBOSS-"),
+            sg.Checkbox("Contour", key="-CONTOUR-"),
+        ],
+        [sg.Checkbox("Flip x", key="-FLIPX-"), sg.Checkbox("Flip y", key="-FLIPY-")],
+        [sg.Button("Save Image", key="-SAVE-")],
+    ]
+)
+image_col = []
+
+layout = [[control_col, image_col]]
 
 window = sg.Window("Image Editor", layout)
 
